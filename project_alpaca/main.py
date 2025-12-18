a10167e5dd7abb9f8a93449bdf18164d43de0f8a
Lucas Miller <lucas.miller1@ucalgary.ca>
2025-12-18 12:04:34 -0700

    small update

--- project_alpaca/main.py
+++ project_alpaca/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
 from datetime import datetime, timedelta
 
 # Use python -m streamlit run main.py to run the app
